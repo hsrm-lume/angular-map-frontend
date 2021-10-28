6ba7777de82ffc12277fa2dc3a202e9c9b525dd9
mptr <mike@marvin-peter.de>
2021-10-28 12:06:58 +0200

    tabbed

--- src/app/leaflet-map/leaflet-map.component.ts
+++ src/app/leaflet-map/leaflet-map.component.ts
@@ -2,16 +2,14 @@ import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 @Component({
-  selector: 'app-leaflet-map',
-  templateUrl: './leaflet-map.component.html',
-  styleUrls: ['./leaflet-map.component.scss']
+	selector: 'app-leaflet-map',
+	templateUrl: './leaflet-map.component.html',
+	styleUrls: ['./leaflet-map.component.scss'],
 })
 export class LeafletMapComponent implements AfterViewInit {
-  
-  constructor() { }
+	constructor() {}
 
-  ngAfterViewInit(): void {
-     var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-  }
-
+	ngAfterViewInit(): void {
+		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	}
 }
