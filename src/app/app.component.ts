c898a63b5450eb126864901f190b6574df46eb9d
vgehr001 <vincent.gehrmann@student.hs-rm.de>
2021-11-11 00:36:56 +0100

    vertical slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { HostListener, Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'angular-frontend';
+	innerWidth = 0;
+	innerHeight = 0;
+
+	@HostListener('window:resize', ['$event'])
+	onResize(_: any) {
+		this.innerWidth = window.innerWidth;
+		this.innerHeight = window.innerHeight;
+	}
+	get isVertical(): boolean {
+		return this.innerWidth < 1000;
+	}
+	ngOnInit(): void {
+		this.onResize(null);
+	}
 }
--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-slider',
@@ -9,7 +9,17 @@ export class SliderComponent implements OnInit {
 	// TODO Banana in a box
 	dateFrom = new Date('2021-01-01');
 	dateTo = new Date('2021-12-31');
+	innerWidth = 0;
+	innerHeight = 0;
 
+	@HostListener('window:resize', ['$event'])
+	onResize(_: any) {
+		this.innerWidth = window.innerWidth;
+		this.innerHeight = window.innerHeight;
+	}
+	get isVertical(): boolean {
+		return this.innerWidth < 1000;
+	}
 	formatLabel(value: number) {
 		return new Date(value).toLocaleDateString().replace(/\.(?=\w+$)/, '. ');
 	}
@@ -21,5 +31,7 @@ export class SliderComponent implements OnInit {
 		return this.dateTo.toLocaleDateString();
 	}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.onResize(null);
+	}
 }
