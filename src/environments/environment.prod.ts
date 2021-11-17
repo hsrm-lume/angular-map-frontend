270937f15633e9e7beac3f4c3e7bc8c2928b520f
mptr <54647612+mptr@users.noreply.github.com>
2021-11-17 10:58:11 +0100

    neo4j url with mapper

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
 	neo4j: {
-		url: 'neo4j://neo4j.lume.m-ptr.de:7687',
+		url: (location: any) => {
+			if(location.hostname === 'localhost') return "neo4j://localhost";
+			else return `neo4j://neo4j.${location.hostname}:7687`
+		},
 		username: 'readonly',
 		password: 'well-known',
 	},
