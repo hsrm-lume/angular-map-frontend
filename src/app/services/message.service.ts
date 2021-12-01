b6c6566060da1c689c7a6a9ef2baa6b48fd6e62f
mptr <54647612+mptr@users.noreply.github.com>
2021-12-01 21:43:35 +0100

    message service done

---
+++ src/app/services/message.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+interface Message {
+	title: string;
+	text: string;
+	type: 'success' | 'error' | 'warning' | 'info';
+}
+
+@Injectable({
+	providedIn: 'root',
+})
+export class MessageService {
+	constructor() {}
+	ms: Message[] = [];
+	push(m: Message) {
+		// reject duplicates
+		if (this.ms.some((m2) => m2.title === m.title)) return;
+		this.ms.push(m);
+	}
+	remove(m: Message) {
+		this.ms.splice(this.ms.indexOf(m), 1);
+	}
+}
