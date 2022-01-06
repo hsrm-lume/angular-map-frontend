85a285fcdfb52db1851fb2f996f613bbbb3b25ec
mptr <54647612+mptr@users.noreply.github.com>
2022-01-06 09:11:27 +0100

    Hotfix hostname fpr local ips

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
 	neo4j: {
 		url: (location: any) => {
 			if (location.hostname === 'localhost') return 'neo4j://localhost';
-			else return `neo4j://neo4j.${location.hostname}:7687`;
+			else return `neo4j://${location.hostname}:7687`;
 		},
 		username: 'readonly',
 		password: 'well-known',
