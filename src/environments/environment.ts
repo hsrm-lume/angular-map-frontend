17250b57d1ac64c0210a9d01e365b921dd96634a
mptr <54647612+mptr@users.noreply.github.com>
2022-01-19 00:59:37 +0100

    map styles now with self hosted tiles server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
 		password: 's3cr3t4',
 	},
 	startDate: new Date('2021-01-01').getTime(),
+	tileServerUrl: 'http://localhost:8080',
 	appDownloadUrl:
 		'https://github.com/hsrm-lume/react-native-cli-lume/releases/latest/download/app-release.apk',
 	production: false,
