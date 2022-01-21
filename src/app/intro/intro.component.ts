86b94797473e12b32808d746ae7aa1b297de0656
mptr <54647612+mptr@users.noreply.github.com>
2022-01-22 00:31:39 +0100

    intro done

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,8 +1,16 @@
+import {
+	animate,
+	state,
+	style,
+	transition,
+	trigger,
+} from '@angular/animations';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 interface IntroItem {
-	icon: string;
-	headline: string;
+	title: string;
+	image: string;
 	text: string;
 }
 
@@ -18,9 +26,44 @@ export class IntroComponent {
 	currentIndex = 0;
 	messages: IntroItem[] = [
 		{
-			headline: 'Hi!',
-			icon: '/assets/intro/wave.svg',
+			title: 'Hi!',
+			image: 'wave.svg',
 			text: 'lume is an app for sharing a digital Olympic Torch',
+		},
+		{
+			title: 'the torch',
+			image: 'torchFlame.svg',
+			text: 'Every user with the lume app has a torch, that can be lit through interacting with an other lume device having its flame turned on.',
+		},
+		{
+			title: 'receiving a torch',
+			image: 'shareFlame.svg',
+			text: 'There are two ways of interaction.\nYou may just hold two android phones together, and the flame will be passed',
+		},
+		{
+			title: 'using your camera',
+			image: 'shareCamera.svg',
+			text: 'Or use your camera and scan a lume QR code.\nYour torch will be lit in no-time',
+		},
+		{
+			title: 'sharing your flame',
+			image: 'shareFlameFlipped.svg',
+			text: "Sharing is as simple as receiving a flame, just hold two android phones together and your phone will share it's flame with the recipient",
+		},
+		{
+			title: 'sharing your flame',
+			image: 'shareQr.svg',
+			text: 'Alternatively press the QR code button to share your fire with another person',
+		},
+		{
+			title: 'Are you hungry for more?',
+			image: 'map.svg',
+			text: 'View lume torch stats with this map view.\nHow many times has it been passed on?\n Where has the torch gone?\nHave fun exploring the map!',
+		},
+		{
+			title: 'Be a part of lume!',
+			image: 'torchFlame.svg',
+			text: 'To become part of the lume network, just download the app to recieve and spread the fire!',
 		},
 	];
 	get currentSlide() {
@@ -37,5 +80,8 @@ export class IntroComponent {
 	close() {
 		console.log('close');
 		this.toggleIntro.emit();
+	}
+	download() {
+		window.location.href = environment.appDownloadUrl;
 	}
 }
